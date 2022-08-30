e4a999e605f08b9931d62f3f24681592640bd207
PabloJRW <ramoswilkinspj28@gmail.com>
2022-08-29 21:22:52 -0500

    reestructura del repositorio

---
+++ auxiliar_functions/plotDistributions.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# DISTRIBUCIONES DE VARIABLES SIN ESCALAR
+# ==============================================================================
+def plotDistributions(dataframe:pd.DataFrame, title=None):
+    fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(15, 4))
+    axes = axes.flat
+
+    for i, feature in enumerate(dataframe.columns):
+        sns.distplot(
+            x       =dataframe[feature],
+            color   = (list(plt.rcParams['axes.prop_cycle'])*2)[i]["color"],
+            ax      = axes[i]
+        )
+        axes[i].set_title(feature, fontsize = 12, fontweight = "bold")
+        axes[i].tick_params(labelsize = 9)
+        axes[i].set_xlabel("")
+    
+    fig.tight_layout()
+    fig.suptitle(title, y=1.1,fontsize = 15, fontweight = "bold");
