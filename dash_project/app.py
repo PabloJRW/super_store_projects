d4a78fd46213fab630c9ca0ffa759f8c096e9727
PabloJRW <ramoswilkinspj28@gmail.com>
2023-06-01 17:17:06 -0500

    project 1.1

--- dash_project/app.py
+++ dash_project/app.py
@@ -9,6 +9,9 @@ app = Dash(__name__, external_stylesheets=['styles.css'])
 
 DATA_PATH = os.path.join('..','datasets','raw','Superstore.csv')
 store_df = pd.read_csv(DATA_PATH, parse_dates=['Order Date', 'Ship Date'], encoding='latin-1')
+min_date = store_df['Order Date'].min()
+max_date = store_df['Order Date'].max()
+
 
 app.layout = html.Div(id='main-div',children=[
     #DIV - HEADER
@@ -34,33 +37,37 @@ app.layout = html.Div(id='main-div',children=[
                      ]
                  ),
 
-                 html.Div(id='date-range-picker',
+                 html.Div(
                           className='selector',
                           children=[
-                              html.H3(className='h3-selector'),
-                              dcc.DatePickerRange(
-                                  start_date=datetime(2017,1,1),
-                                  end_date=datetime(2017,12,31),
-                                  min_date_allowed=datetime(2016,1,1),
-                                  max_date_allowed=datetime(2017,12,31),
-                                  style={'height':'60%'}
+                              html.H3(className='h3-selector',
+                                      children='Select an option:'),
+                              dcc.RadioItems(
+                                  id='culosucio',
+                                  options=['Region', 'Segment', 'Category', 'Sub-Category'],
+                                  value='Region',
+                                  style={'display':'flex'}
                               )
-                            ]
+                          ]
                  ),
 
                  html.Div(
                           className='selector',
-                          children=
-                              dcc.RadioItems(
-                                  id='culosucio',
-                                  options=['Region', 'Category', 'Sub-Category'],
-                                  value='Region',
-                                  style={'display':'flex'}
+                          children=[
+                              html.H3(className='h3-selector',
+                                      children='Select a date range'),
+                              dcc.DatePickerRange(
+                                  id='date-range-picker',
+                                  start_date=min_date,
+                                  end_date=max_date,
+                                  min_date_allowed=min_date,
+                                  max_date_allowed=max_date,
+                                  style={'height':'60%'}
                               )
-                 )
-
+                            ]
+                 ),
              ]
-    ),
+    ), # DIV SELECTORS FINAL
     
     html.Div(id='div-graphs',
              children=[
@@ -74,44 +81,32 @@ app.layout = html.Div(id='main-div',children=[
                                             }
                             )
                         ]
-                    ),   
-     
-                    # DIV - DUAL VIZ
-                    html.Div(
-                        className='div-viz',
-                        children = [
-                            html.Div(className='bi-viz',
-                                     children=[
-                                        dcc.Graph(id='2nd-graph')
-                                     ]
-                            ),
-
-                            html.Div(className='bi-viz',
-                                children=[
-                                    dcc.Graph(id='3rd-graph')
-                                ]
-                            )
-        ]
-    )    
+                    ),
              ])
 
 ], style={'overflow-y':'hidden'})
 
 
-@app.callback(Output('2nd-graph', 'figure'),
+@app.callback(Output('1st-graph', 'figure'),
               [Input('culosucio', 'value'),
-               Input('picoloco', 'value')])
-def groupingData(group_by, option23):
+               Input('picoloco', 'value'),
+               Input('date-range-picker', 'start_date'),
+               Input('date-range-picker', 'end_date')])
+def groupingData(group_by, option23, start_date, end_date):
     """Function that group data by 'group_by' and return percentage variables"""
     
+    store_df.sort_values(by='Order Date', inplace=True)
+    #Select date range
+    date_range_df = store_df[(store_df['Order Date']>=start_date)&(store_df['Order Date']<=end_date)]
     # Grouping data by feat
-    grouping = store_df.groupby(by=group_by).sum().drop('Postal Code', axis=1)
+    grouping = date_range_df.groupby(by=group_by).sum().drop('Postal Code', axis=1)
     
     fig = px.bar(x=grouping.index, 
                   y=grouping[option23])
     
     fig.update_layout(
-        title="{} by {}".format(option23, group_by),
+        title={'text':"{} BY {}".format(option23.upper(), group_by.upper()),
+               'x':0.5},
         xaxis_title="{}".format(group_by),
         yaxis_title="{}".format(option23)
     )
