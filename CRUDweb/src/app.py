6c70518567138abb7c56b9fc0023aa7b41ba776f
PabloJRW <ramoswilkinspj28@gmail.com>
2023-06-04 20:25:13 -0500

    crud web application

---
+++ CRUDweb/src/app.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask, render_template
+
+
+
+app = Flask(__name__)
+
+template_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+template_dir = os.path.join(template_dir, 'src', 'templates')
+
+
+app = Flask(__name__, template_folder=template_dir)
+
+# Rutas de la aplicación 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5050)
