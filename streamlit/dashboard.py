4fc37e0a0208c28deedd5a20681b2dcd6ac66a84
Pablo <ramoswilkinspj28@gmail.com>
2023-07-11 17:49:39 -0500

    streamlit

--- streamlit/dashboard.py
+++ streamlit/dashboard.py
@@ -6,7 +6,8 @@ import streamlit as st
 pd.set_option('display.float_format','{:.2f}'.format)
 DATA_PATH = os.path.join('datasets','raw','Superstore.csv')
 data = pd.read_csv(DATA_PATH, parse_dates=['Order Date', 'Ship Date'], encoding='latin-1')
-data_by_region = data.groupby('Region')[['Sales', 'Profit']].mean()
+data_by_region = data.groupby('Region')[['Sales', 'Profit']].sum()
+data_by_segment = data.groupby('Segment')[['Sales', 'Profit']].sum()
 
 
 st.title("SuperStore Sales Dashboard")
@@ -25,5 +26,14 @@ max_date = st.sidebar.date_input(
     datetime.date(2021, 7, 6))
 
 
-st.subheader("By Region")
-st.bar_chart(data_by_region[options])
+st.line_chart(data=data, x='Order Date', y='Sales')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.subheader("By Region")
+    st.bar_chart(data_by_region[options])
+
+with col2:
+    st.subheader("By Segment")
+    st.bar_chart(data_by_segment[options])
