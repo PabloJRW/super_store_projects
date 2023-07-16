1eae18527c4bcae62e0dac9cd3fd2054066835d5
Pablo <ramoswilkinspj28@gmail.com>
2023-07-15 20:14:26 -0500

    ok

--- streamlit/Home.py
+++ streamlit/Home.py
@@ -34,6 +34,8 @@ min_year_selected, max_year_selected = st.sidebar.select_slider(
     options=list_of_years,
     value=(list_of_years.min(), list_of_years.max()))
 
+# Line chart view mode
+checkbox_value = st.sidebar.checkbox("Line Chart View", value=True)
 
 #  #######################################################################################
 
@@ -46,24 +48,58 @@ resample_data = filtered_by_year.resample('M').sum()
 
 # DASHBOARD ############################################################################################
 
+
+months_labels = ['January','February','March','April','May','June','July','August','September','October','November','December']
+
 # Line chart +++++++++++++
-fig = go.Figure()
-for year in range(min_year_selected, max_year_selected+1):
-    fig.add_trace(go.Scatter(y=resample_data[resample_data.index.year == year][options],
-                             mode='lines',
-                             name=f"{options} {year}",
+
+if checkbox_value:
+
+    fig = go.Figure()
+    for year in range(min_year_selected, max_year_selected+1):
+        fig.add_trace(go.Scatter(y=resample_data[resample_data.index.year == year][options],
+                                 mode='lines',
+                                 name=year))
+
+    fig.update_layout(title=f"{options} by year",
+                      xaxis_title="Month",
+                      yaxis_title=f"$ {options}",
+                      legend_title="Legend Title",
+                      xaxis=dict(
+                          tickvals=[1,2,3,4,5,6,7,8,9,10,11,12],
+                          ticktext=months_labels
+                        )
+                  
+    )
+    st.plotly_chart(fig)
+
+else:
+    for year in range(min_year_selected, max_year_selected+1):
+        fig = go.Figure(go.Scatter(y=resample_data[(resample_data.index.year >= min_year_selected) & (resample_data.index.year <= max_year_selected)][options],
+                             mode='lines'
                             ))
+        
+        fig.update_layout(title=f"{options} by year",
+                      xaxis_title="Month",
+                      yaxis_title=f"$ {options}",
+                      legend_title="Legend Title",
+                        )
+                  
 
 
-st.plotly_chart(fig)
+    st.plotly_chart(fig)
+        
 
 col1, col2 = st.columns(2)
 
-
+fig_by_region = go.Figure(go.Bar(x=data_by_region.index, y=data_by_region[options]))
 with col1:
     st.subheader(f"{options} by Region")
-    st.bar_chart(data_by_region[options])
+    st.plotly_chart(fig_by_region, use_container_width=True)
+    
 
+fig_by_segment = go.Figure(go.Bar(x=data_by_segment.index, y=data_by_segment[options]))
 with col2:
     st.subheader(f"{options} by Segment")
-    st.bar_chart(data_by_segment[options])
+    st.plotly_chart(fig_by_segment, use_container_width=True)
+
