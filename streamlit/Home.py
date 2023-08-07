43eeb4bac101dae84ed5ebcb53d0888ec148a218
Pablo <ramoswilkinspj28@gmail.com>
2023-08-06 20:53:19 -0500

    resumen actualizado

--- streamlit/Home.py
+++ streamlit/Home.py
@@ -14,7 +14,7 @@ data.set_index('Order Date', inplace=True)
 st.set_page_config(page_title="SuperStore")
 
 # Title of dashboard
-st.title("SuperStore Sales Dashboard")
+st.title("Tienda Tipo Walmart")
 
 
 # SIDEBAR #########################################################################################
@@ -34,8 +34,7 @@ min_year_selected, max_year_selected = st.sidebar.select_slider(
     options=list_of_years,
     value=(list_of_years.min(), list_of_years.max()))
 
-# Line chart view mode
-checkbox_value = st.sidebar.checkbox("Line Chart View", value=True)
+
 
 #  #######################################################################################
 
@@ -51,44 +50,48 @@ resample_data = filtered_by_year.resample('M').sum()
 
 months_labels = ['January','February','March','April','May','June','July','August','September','October','November','December']
 
-# Line chart +++++++++++++
+# Line chart view mode
+checkbox_value = st.sidebar.checkbox("Line Chart View", value=True)
 
+
+
+# Line chart +++++++++++++
 if checkbox_value:
 
     fig = go.Figure()
     for year in range(min_year_selected, max_year_selected+1):
         fig.add_trace(go.Scatter(y=resample_data[resample_data.index.year == year][options],
-                                 mode='lines',
-                                 name=year))
+                                mode='lines',
+                                name=year))
 
     fig.update_layout(title=f"{options} by year",
-                      xaxis_title="Month",
-                      yaxis_title=f"$ {options}",
-                      legend_title="Legend Title",
-                      xaxis=dict(
-                          tickvals=[1,2,3,4,5,6,7,8,9,10,11,12],
-                          ticktext=months_labels
+                    xaxis_title="Month",
+                    yaxis_title=f"$ {options}",
+                    legend_title="Legend Title",
+                    xaxis=dict(
+                        tickvals=[1,2,3,4,5,6,7,8,9,10,11,12],
+                        ticktext=months_labels
                         )
-                  
+                
     )
     st.plotly_chart(fig)
 
 else:
     for year in range(min_year_selected, max_year_selected+1):
         fig = go.Figure(go.Scatter(y=resample_data[(resample_data.index.year >= min_year_selected) & (resample_data.index.year <= max_year_selected)][options],
-                             mode='lines'
+                            mode='lines'
                             ))
         
         fig.update_layout(title=f"{options} by year",
-                      xaxis_title="Month",
-                      yaxis_title=f"$ {options}",
-                      legend_title="Legend Title",
+                    xaxis_title="Month",
+                    yaxis_title=f"$ {options}",
+                    legend_title="Legend Title",
                         )
-                  
+                
 
 
     st.plotly_chart(fig)
-        
+   
 
 col1, col2 = st.columns(2)
 
